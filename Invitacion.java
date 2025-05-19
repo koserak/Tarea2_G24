9d420ce5f9bd1082c03c6e7b6f533fadc88857ab
FurioStyle <benjamin.bglh@gmail.com>
2025-05-19 14:45:02 -0400

    feat: esqueleto del codigo

--- Invitacion.java
+++ Invitacion.java
@@ -3,4 +3,13 @@ import java.time.Instant;
 public class Invitacion {
 
     private Instant hora;
+
+    public Invitacion(Instant h){
+        this.hora = h;
+    }
+
+    public String getHora() {
+        return "La invitacion fue enviada a las" + hora;
+    }
+
 }
