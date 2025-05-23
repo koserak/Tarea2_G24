c03e5d81a0a45c2f781806005926cea8d43242f7
FurioStyle <benjamin.bglh@gmail.com>
2025-05-23 18:19:33 -0400

    feat: Agregada Documentacion Parcial

--- Nota.java
+++ Nota.java
@@ -1,14 +1,25 @@
 import java.time.LocalTime;
 
+/**
+ * Clase que representa una nota que se agregara al final de la reunion en el informe.
+ */
 public class Nota {
     private String contenido;
     private LocalTime hora;
 
+    /**
+     * Constructor de la nota.
+     * @param contenido String con el contenido de esta.
+     */
     public Nota(String contenido) {
         this.contenido = contenido;
         this.hora = LocalTime.now();
     }
 
+    /**
+     * Getter para obtener el contenido de la nota.
+     * @return String con el contenido de la nota.
+     */
     public String getContenido() {
         return contenido;
     }
