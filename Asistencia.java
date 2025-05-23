899db3d7f85f2906363bd9fd8c5888dc6cf2f1d9
FurioStyle <benjamin.bglh@gmail.com>
2025-05-23 18:16:50 -0400

    feat: Agregado Documentacion

--- Asistencia.java
+++ Asistencia.java
@@ -1,20 +1,32 @@
 import java.time.Instant;
 
-
+/**
+ * Clase para marcar asistencia de los empleados, guarda el nombre y la hora a la que este llega.
+ */
 public class Asistencia {
     private Empleado empleado;
     private Instant horaLlegada;
 
+    /**
+     * Constructor que guarda los datos del empleado y su Fecha de llegada.
+     */
     public Asistencia(Empleado empleado, Instant horaLlegada) {
         this.empleado = empleado;
         this.horaLlegada = horaLlegada;
     }
 
-
+    /**
+     * Getter para empleado.
+     * @return Empleado y sus datos.
+     */
     public Empleado getEmpleado() {
         return empleado;
     }
 
+    /**
+     * Getter para la hora de llegada.
+     * @return La hora de llegada del empleado.
+     */
     public Instant getHoraLlegada() {
         return horaLlegada; }
 }
