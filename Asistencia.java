0ddf7952b2860e7ba90055cfd324bac74b866d1a
FurioStyle <benjamin.bglh@gmail.com>
2025-05-23 17:54:54 -0400

    update: trabajando en base al codigo de Ariel

--- Asistencia.java
+++ Asistencia.java
@@ -1,2 +1,20 @@
+import java.time.Instant;
+
+
 public class Asistencia {
+    private Empleado empleado;
+    private Instant horaLlegada;
+
+    public Asistencia(Empleado empleado, Instant horaLlegada) {
+        this.empleado = empleado;
+        this.horaLlegada = horaLlegada;
+    }
+
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public Instant getHoraLlegada() {
+        return horaLlegada; }
 }
