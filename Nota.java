c9cdb83b0b8388383f096291536dccd97e7ff525
FurioStyle <benjamin.bglh@gmail.com>
2025-05-23 18:15:56 -0400

    update: trabajando en base al codigo de Ariel

--- Nota.java
+++ Nota.java
@@ -1,12 +1,24 @@
+import java.time.LocalTime;
+
 public class Nota {
-
     private String contenido;
+    private LocalTime hora;
 
-    public Nota(String c){
-        this.contenido = c;
+    public Nota(String contenido) {
+        this.contenido = contenido;
+        this.hora = LocalTime.now();
     }
 
     public String getContenido() {
         return contenido;
+    }
+
+    public String getHora(){
+        return hora.getHour()+":"+ hora.getMinute()+":"+hora.getSecond();
+    }
+
+    @Override
+    public String toString() {
+        return hora + ": " + contenido;
     }
 }
